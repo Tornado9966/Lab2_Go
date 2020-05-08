f7ad89c53bf20f417aae11eca9b1892e30842a81
Iluysha <ilua.mix12@gmail.com>
2020-05-08 17:01:12 +0300

    Add benchtime

--- build/binmodule/binary.go
+++ build/binmodule/binary.go
@@ -16,7 +16,7 @@ var (
 	}, "workDir", "outputPath", "pkg")
 
 	goTest = pctx.StaticRule("gotest", blueprint.RuleParams{
-		Command:     "cd $workDir && go test -v -bench=. $pkgTest > $outputFile",
+		Command:     "cd $workDir && go test -v -bench=. -benchtime=100x $pkgTest > $outputFile",
 		Description: "test $pkgTest",
 	}, "workDir", "pkgTest", "outputFile")
 
